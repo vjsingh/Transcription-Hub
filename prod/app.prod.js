dd8fd3da7a7015a2c5f093f3a83b6b89cde73ade
vjsingh <varun101@gmail.com>
2012-01-06 18:51:11 -0500

    app.js --> server.js

--- prod/app.prod.js
+++ prod/app.prod.js
@@ -1,3 +1,3 @@
 process.env.NODE_ENV = 'production';
 
-var app = require('../app');
+var app = require('../server');
--- test/app.test.js
+++ test/app.test.js
@@ -1,3 +1,3 @@
 process.env.NODE_ENV = 'test';
 
-var app = require('../app');
+var app = require('../server');
