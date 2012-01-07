888eec4cf785f2a8b57e25f53d49753456e8c958
vjsingh <varun101@gmail.com>
2012-01-07 11:32:42 -0500

    changed port to 80

--- server.js
+++ server.js
@@ -369,7 +369,7 @@ app.get('/transcriptions', member, function(req, res) {
   */
 });
 
-app.listen(13458);
+app.listen(80);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
 // CRUD for transcriptions
